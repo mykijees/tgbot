80cf40612990f0e1444926292fd5d57d6fe911c9
mykijees <51881556+mykijees@users.noreply.github.com>
2020-10-02 23:11:22 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -2,11 +2,11 @@ from tg_bot.sample_config import Config
 
 
 class Development(Config):
-    OWNER_ID = 254318997  # my telegram ID
-    OWNER_USERNAME = "SonOfLars"  # my telegram username
-    API_KEY = "your bot api key"  # my api key, as provided by the botfather
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/database'  # sample db credentials
-    MESSAGE_DUMP = '-1234567890' # some group chat that your bot is a member of
+    OWNER_ID = 1311253220  # my telegram ID
+    OWNER_USERNAME = "chatm3here"  # my telegram username
+    API_KEY = "1315893535:AAFlMQFvb_UFCXnPHdqN1Iu9FTczZ3Fu-EU"  # my api key, as provided by the botfather
+    SQLALCHEMY_DATABASE_URI = 'postgres://ciribot:ciribot@54.254.204.75:5432/ciridata'  # sample db credentials
+    MESSAGE_DUMP = '-440523709' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
     SUDO_USERS = [18673980, 83489514]  # List of id's for users which have sudo access to the bot.
     LOAD = []
